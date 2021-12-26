73499ed8583673bd52c9ccfb62577a2e4a865f29
Matilda Mared <matildamared@live.se>
2021-12-26 14:10:46 +0100

    complete exercise 4.19

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+const getTokenFrom = (request) => {
+	const authorization = request.get("authorization");
+	if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+		return authorization.substring(7);
+	}
+	return null;
+};
 
 blogsRouter.get("/", async (request, response) => {
 	try {
@@ -16,17 +25,26 @@ blogsRouter.get("/", async (request, response) => {
 
 blogsRouter.post("/", async (request, response, next) => {
 	try {
-		if (!request.body.title || !request.body.url) {
+		const body = request.body;
+
+		if (!body.title || !body.url) {
 			return response.status(400).end();
 		}
 
-		const user = await User.findOne({});
+		const token = getTokenFrom(request);
+		const decodedToken = jwt.verify(token, process.env.SECRET);
+
+		if (!token || !decodedToken.id) {
+			return response.status(401).json({ error: "token missing or invalid" });
+		}
+
+		const user = await User.findById(decodedToken.id);
 
 		const blog = new Blog({
-			title: request.body.title,
-			author: request.body.author,
-			url: request.body.url,
-			likes: request.body.likes || 0,
+			title: body.title,
+			author: body.author,
+			url: body.url,
+			likes: body.likes || 0,
 			user: user._id,
 		});
 
